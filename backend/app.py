cf105a53b25a2377973eb7c5e0e0e2181670af51
Anirudh <anirudhpisharady@gmail.com>
2026-07-27 23:10:44 +0530

    Setup registration module and users table

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from database import get_db_connection
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "<h1>Online Banking System</h1>"
+
+    try:
+        connection = get_db_connection()
+
+        if connection.is_connected():
+            return "<h2>Database Connected Successfully!</h2>"
+
+    except Exception as e:
+        return f"Database Connection Failed:<br>{e}"
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
